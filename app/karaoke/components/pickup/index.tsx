a6e577b908967da903539a67a9abdc48edacfacc
Andrei Filip <andreifilip123@gmail.com>
2025-05-14 14:17:28 +0300

    add lid

--- app/karaoke/components/pickup/index.tsx
+++ app/karaoke/components/pickup/index.tsx
@@ -16,11 +16,25 @@ export default function Pickup({
         <div className="side bottom" />
         <div className="side left" />
         <div className="side right" />
-        <div className="side front" />
+        <div className="side front">
+          <div className="controls">
+            <button onClick={onPlay}>Play</button>
+            <button onClick={onPause}>Pause</button>
+            <button onClick={onStop}>Stop</button>
+          </div>
+        </div>
         <div className="side back" />
+        <div className="plate" />
+        <div className="recordSupport" />
+        <div className="lid">
+          <div className="side top" />
+          <div className="side bottom" />
+          <div className="side left" />
+          <div className="side right" />
+          <div className="side front" />
+        </div>
       </div>
-      {/* <div className="plate" />
-      <div className="record" />
+      {/* <div className="record" />
       <div className="needle" />
       <div className="controls" /> */}
     </div>
