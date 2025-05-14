4c5c25bb3f73fa790fd86f632b054ade82798205
Andrei Filip <andreifilip123@gmail.com>
2025-05-14 22:28:43 +0300

    add simple logic for spin/rotate/lift

--- app/karaoke/components/pickup/index.tsx
+++ app/karaoke/components/pickup/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import "./style.scss";
 
 export default function Pickup({
@@ -9,6 +10,35 @@ export default function Pickup({
   onPause: () => void;
   onStop: () => void;
 }) {
+  const [needleRotated, setNeedleRotated] = useState(false);
+  const [needleLifted, setNeedleLifted] = useState(false);
+  const [spinning, setSpinning] = useState(false);
+
+  const onPlaying = useCallback(() => {
+    setNeedleRotated(true);
+    setSpinning(true);
+    onPlay();
+  }, [onPlay]);
+
+  const onPaused = useCallback(() => {
+    setNeedleLifted(true);
+    onPause();
+  }, [onPause]);
+
+  const onStopped = useCallback(() => {
+    setNeedleLifted(true);
+    setTimeout(() => {
+      setNeedleRotated(false);
+    }, 1000);
+    setTimeout(() => {
+      setSpinning(false);
+    }, 3000);
+    setTimeout(() => {
+      setNeedleLifted(false);
+    }, 4000);
+    onStop();
+  }, [onStop]);
+
   return (
     <div className="scene">
       <div className="box base">
@@ -20,21 +50,21 @@ export default function Pickup({
           <div className="controls">
             <div className="control">
               <span className="label">Play</span>
-              <button onClick={onPlay} />
+              <button onClick={onPlaying} />
             </div>
             <div className="control">
               <span className="label">Pause</span>
-              <button onClick={onPause} />
+              <button onClick={onPaused} />
             </div>
             <div className="control">
               <span className="label">Stop</span>
-              <button onClick={onStop} />
+              <button onClick={onStopped} />
             </div>
           </div>
         </div>
         <div className="side back" />
-        <div className="plate" />
-        <div className="recordSupport" />
+        <div className={`plate ${spinning ? "spinning" : ""}`} />
+        <div className={`recordSupport ${spinning ? "spinning" : ""}`} />
         <div className="box lid">
           <div className="side top" />
           <div className="side left" />
@@ -42,6 +72,9 @@ export default function Pickup({
           <div className="side front" />
           <div className="side back" />
         </div>
+        <div
+          className={`needle ${needleRotated ? "rotated" : ""} ${needleLifted ? "lifted" : ""}`}
+        />
       </div>
       {/* <div className="record" />
       <div className="needle" />
