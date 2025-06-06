76ec025a2c907c5a237090fd7b64ed85b43a7387
Andrei Filip <andreifilip123@gmail.com>
2025-06-06 14:27:13 +0300

    format all

--- app/utils/convertToPascalCase.ts
+++ app/utils/convertToPascalCase.ts
@@ -1,9 +1,9 @@
 export const convertToPascalCase = (s: string) => {
   if (!s) return s;
   return s
-    .split(' ')
+    .split(" ")
     .map((word) => {
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     })
-    .join('');
+    .join("");
 };
