a413e3142feadeca0dfb1012f8beacbbb5eeebe2
daudkresna <daudkresna@gmail.com>
2025-11-11 19:03:35 +0700

    fix: fix transitions on card page
    fix the ViewTransition on the card, card overview, card slug page

--- app/card/[slug]/page-client.tsx
+++ app/card/[slug]/page-client.tsx
@@ -1,84 +1,101 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { ViewTransition } from 'react'
-import { type Place } from '@/app/utils/constants'
+import Image from "next/image";
+import Link from "next/link";
+import { ViewTransition } from "react";
+import { type Place } from "@/app/utils/constants";
 
-export default function Place({ place, places }: { place: Place; places: Place[] }) {
+export default function Place({
+  place,
+  places,
+}: {
+  place: Place;
+  places: Place[];
+}) {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="min-h-screen flex items-center justify-center flex-col md:flex-row w-full px-0 md:px-8">
-        <div className="relative w-full sm:w-auto group">
-          <ViewTransition name="back-button">
-            <Link
-              href="/card"
-              className="absolute top-4 left-4 text-gray-100 bg-opacity-50 py-1 rounded-full hover:bg-opacity-75 transition-colors z-10 filter drop-shadow-[2px_2px_6px_#666]"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <ViewTransition>
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center flex-col md:flex-row w-full px-0 md:px-8">
+          <div className="relative w-full sm:w-auto group">
+            <ViewTransition name="back-button">
+              <Link
+                href="/card"
+                className="absolute top-4 left-4 text-gray-100 bg-opacity-50 py-1 rounded-full hover:bg-opacity-75 transition-colors z-10 filter drop-shadow-[2px_2px_6px_#666]"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-              </svg>
-            </Link>
-          </ViewTransition>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                  />
+                </svg>
+              </Link>
+            </ViewTransition>
 
-          <ViewTransition name={`place-image-main`}>
-            <div className="relative md:rounded-lg overflow-clip transition-all duration-300 ease-in-out cursor-pointer w-full y-full md:w-[70vw] h-[70vh]">
-              <Image
-                loading="eager"
-                decoding="sync"
-                src={place.image}
-                alt={place.name}
-                fill
-                className="object-cover overflow-clip md:rounded-lg transition-all duration-300 ease-in-out group-hover:scale-105"
-              />
-            </div>
-          </ViewTransition>
+            <ViewTransition name={`place-image-main`}>
+              <div className="relative md:rounded-lg overflow-clip transition-all duration-300 ease-in-out cursor-pointer w-full y-full md:w-[70vw] h-[70vh]">
+                <Image
+                  loading="eager"
+                  decoding="sync"
+                  src={place.image}
+                  alt={place.name}
+                  fill
+                  className="object-cover overflow-clip md:rounded-lg transition-all duration-300 ease-in-out group-hover:scale-105"
+                />
+              </div>
+            </ViewTransition>
 
-          <ViewTransition name="place-name">
-            <div className="absolute w-full h-full inset-0 md:rounded-lg overflow-clip transition-all duration-300 ease-out filter drop-shadow-[2px_2px_12px_#333]">
-              <div className="absolute bottom-4 right-4 text-gray-100 bg-opacity-50 rounded-xl text-3xl drop-shadow-xs">
-                {place.name}
+            <ViewTransition name="place-name">
+              <div className="absolute w-full h-full inset-0 md:rounded-lg overflow-clip transition-all duration-300 ease-out filter drop-shadow-[2px_2px_12px_#333]">
+                <div className="absolute bottom-4 right-4 text-gray-100 bg-opacity-50 rounded-xl text-3xl drop-shadow-xs">
+                  {place.name}
+                </div>
+              </div>
+            </ViewTransition>
+          </div>
+
+          <ViewTransition name="right-side-bar">
+            <div className="w-full my-8 p-2 md:w-auto md:ml-8 md:p-8">
+              <h3 className="hidden sm:block text-xl font-medium mb-6">
+                Spots
+              </h3>
+              <div className="space-y-4">
+                {places.map((place) => (
+                  <Link
+                    key={place.id}
+                    href={`/card/${place.slug}`}
+                    className="flex items-center gap-4 p-2 transition-colors"
+                  >
+                    <div className="relative w-20 h-20 shrink-0">
+                      <ViewTransition name={`place-image-${place.slug}`}>
+                        <Image
+                          loading="eager"
+                          decoding="sync"
+                          src={place.image}
+                          alt={place.name}
+                          fill
+                          className="object-cover transition-transform filter hover:scale-105 overflow-hidden rounded-lg flex-1"
+                        />
+                        {/* name label */}
+                      </ViewTransition>
+                    </div>
+                    <div className="text-gray-700 bg-opacity-50 rounded-xl text-sm">
+                      {place.name}
+                    </div>
+                  </Link>
+                ))}
               </div>
             </div>
           </ViewTransition>
         </div>
-
-        <ViewTransition name="right-side-bar">
-          <div className="w-full my-8 p-2 md:w-auto md:ml-8 md:p-8">
-            <h3 className="hidden sm:block text-xl font-medium mb-6">Spots</h3>
-            <div className="space-y-4">
-              {places.map((place) => (
-                <Link
-                  key={place.id}
-                  href={`/card/${place.slug}`}
-                  className="flex items-center gap-4 p-2 transition-colors"
-                >
-                  <div className="relative w-20 h-20 shrink-0">
-                    <ViewTransition name={`place-image-${place.slug}`}>
-                      <Image
-                        loading="eager"
-                        decoding="sync"
-                        src={place.image}
-                        alt={place.name}
-                        fill
-                        className="object-cover transition-transform filter hover:scale-105 overflow-hidden rounded-lg flex-1"
-                      />
-                      {/* name label */}
-                    </ViewTransition>
-                  </div>
-                  <div className="text-gray-700 bg-opacity-50 rounded-xl text-sm">{place.name}</div>
-                </Link>
-              ))}
-            </div>
-          </div>
-        </ViewTransition>
       </div>
-    </div>
-  )
+    </ViewTransition>
+  );
 }
--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { ViewTransition } from 'react'
-import { PLACES } from '../utils/constants'
-import { DynamicBackground } from '@/components/ui/dynamic-background'
+import Image from "next/image";
+import Link from "next/link";
+import { ViewTransition } from "react";
+import { PLACES } from "../utils/constants";
+import { DynamicBackground } from "@/components/ui/dynamic-background";
 
 function LeftSideMenu() {
   return (
@@ -22,7 +22,6 @@ function LeftSideMenu() {
             </Link>
           </div>
         </div>
-        
 
         {/* sticker icon */}
         <div className="flex items-center gap-2 text-gray-800 mb-4 z-10">
@@ -37,58 +36,64 @@ function LeftSideMenu() {
         <div className="flex-1 flex flex-col justify-center z-10">
           <h1 className="text-6xl font-serif space-y-2">
             <div>
-              Explore <span className="text-4xl font-serif block md:inline-block">The cities.</span>
+              Explore{" "}
+              <span className="text-4xl font-serif block md:inline-block">
+                The cities.
+              </span>
             </div>
           </h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default function Page() {
   return (
-    // make this locate on top of the page when it's on mobile
-    <div className="flex flex-col md:flex-row min-h-screen ">
-      {/* Left Section */}
-      <ViewTransition name="left-side-bar">
-        <LeftSideMenu />
-      </ViewTransition>
+    <ViewTransition>
+      <div className="flex flex-col md:flex-row min-h-screen ">
+        {/* Left Section */}
+        <ViewTransition name="left-side-bar">
+          <LeftSideMenu />
+        </ViewTransition>
 
-      {/* Right Section */}
-      <ViewTransition name="right-side-bar">
-        <div className="w-full md:w-1/2 p-2 md:p-8">
-          <h2 className="text-xl font-medium px-2 text-gray-700 mt-4">Spots</h2>
-          <div className="space-y-4 flex gap-8 p-2 flex-wrap">
-            {PLACES.map((place) => (
-              <Link
-                key={place.id}
-                href={`/card/${place.slug}`}
-                className="hover:bg-gray-50 transition-colors w-full md:w-[300px] h-[300px] items-stretch"
-              >
-                <div className="relative w-full h-full flex-shrink-0 overflow-clip rounded-lg group">
-                  <ViewTransition name={`place-image-${place.slug}`}>
-                    <Image
-                      loading="eager"
-                      decoding="sync"
-                      src={place.image || '/placeholder.svg'}
-                      alt={place.name}
-                      fill
-                      className="object-cover flex-1 transition-transform  overflow-clip rounded-lg group-hover:scale-110"
-                    />
-                  </ViewTransition>
-                  {/* name label */}
-                  <ViewTransition name={`place-name-${place.slug}`}>
-                    <div className="absolute bottom-4 right-4 text-gray-100 bg-opacity-50 rounded-xl text-3xl filter [text-shadow:0px_0px_8px_#111]">
-                      {place.name}
-                    </div>
-                  </ViewTransition>
-                </div>
-              </Link>
-            ))}
+        {/* Right Section */}
+        <ViewTransition name="right-side-bar">
+          <div className="w-full md:w-1/2 p-2 md:p-8">
+            <h2 className="text-xl font-medium px-2 text-gray-700 mt-4">
+              Spots
+            </h2>
+            <div className="space-y-4 flex gap-8 p-2 flex-wrap">
+              {PLACES.map((place) => (
+                <Link
+                  key={place.id}
+                  href={`/card/${place.slug}`}
+                  className="hover:bg-gray-50 transition-colors w-full md:w-[300px] h-[300px] items-stretch"
+                >
+                  <div className="relative w-full h-full flex-shrink-0 overflow-clip rounded-lg group">
+                    <ViewTransition name={`place-image-${place.slug}`}>
+                      <Image
+                        loading="eager"
+                        decoding="sync"
+                        src={place.image || "/placeholder.svg"}
+                        alt={place.name}
+                        fill
+                        className="object-cover flex-1 transition-transform  overflow-clip rounded-lg group-hover:scale-110"
+                      />
+                    </ViewTransition>
+                    {/* name label */}
+                    <ViewTransition name={`place-name-${place.slug}`}>
+                      <div className="absolute bottom-4 right-4 text-gray-100 bg-opacity-50 rounded-xl text-3xl filter [text-shadow:0px_0px_8px_#111]">
+                        {place.name}
+                      </div>
+                    </ViewTransition>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-      </ViewTransition>
-    </div>
-  )
+        </ViewTransition>
+      </div>
+    </ViewTransition>
+  );
 }
